026bfda70c38d1210f24de913f6cdb1087a8e3f0
weijiang <jw.adaptivealgo@gmail.com>
2017-06-01 11:50:50 -0400

    image generator script with correct dir path

--- AdaptiveAlgo/private/services/dockerimage_generator.py
+++ AdaptiveAlgo/private/services/dockerimage_generator.py
@@ -1,14 +1,24 @@
 #under Python 2.7
 import subprocess
 import json
+import os
 
 #parse JSON file
-with open('master.json') as data_file:    
+service_dir = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+with open(service_dir+'/master.json') as data_file:    
     data = json.load(data_file)
 
+#get basic info from data
+module_name = 'finace modeling'.replace(" ", "")
 num_pkg_py27 = len(data['data'][0]['packages'])
 num_pkg_py35 = len(data['data'][1]['packages'])
 num_pkg_r = len(data['data'][2]['packages'])
+
+#create module dir to save Dockerfile
+module_dir = service_dir + module_name
+if not os.path.exists(module_dir):
+    os.makedirs(module_dir)
+print(module_dir)
 
 if num_pkg_py27 > 0 and num_pkg_r > 0:
     baseImage = 'jupyterhub/base-pyr'
@@ -20,7 +30,7 @@ else:
     baseImage = 'jupyter/base-py35'
 
 #write the Dockerfile
-with open('Dockerfile1', 'wb') as dockerfile:
+with open(module_dir+'/Dockerfile', 'wb') as dockerfile:
     #FROM the base image
     dockerfile.write('FROM ' + baseImage + '\n')
     #get the root privilege 
@@ -31,18 +41,34 @@ with open('Dockerfile1', 'wb') as dockerfile:
         dockerfile.write('RUN pip2 --no-cache-dir --no-cache install ')
         for index,pkg in enumerate(data['data'][0]['packages']):
             if(index < num_pkg_py27-1):
-                dockerfile.write(pkg['name'] + ' ')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write(pkg['name'] + ' ')
+                else:
+                    dockerfile.write(pkg['name'] +'==' +pkg['version']+ ' ')
             else:
-                dockerfile.write(pkg['name'] +'\n')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write(pkg['name'] + '\n')
+                else:
+                    dockerfile.write(pkg['name'] +'==' +pkg['version']+ '\n')
     
     #install py3 packages
     if num_pkg_py35 > 0:
         dockerfile.write('RUN pip3 --no-cache-dir --no-cache install ')
         for index,pkg in enumerate(data['data'][1]['packages']):
             if(index < num_pkg_py35-1):
-                dockerfile.write(pkg['name'] + ' ')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write(pkg['name'] + ' ')
+                else:
+                    dockerfile.write(pkg['name'] +'==' +pkg['version']+ ' ')
             else:
-                dockerfile.write(pkg['name'] +'\n')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write(pkg['name'] + '\n')
+                else:
+                    dockerfile.write(pkg['name'] +'==' +pkg['version']+ '\n')
 
     
     #install r packages
@@ -50,11 +76,19 @@ with open('Dockerfile1', 'wb') as dockerfile:
         dockerfile.write('RUN conda install --quiet --yes \\\n')
         for index,pkg in enumerate(data['data'][2]['packages']):
             if(index < num_pkg_r-1):
-                dockerfile.write('    \'' +pkg['name']+ '\' \\\n')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write('    \'' +pkg['name']+ '\' \\\n')
+                else:
+                    dockerfile.write('    \'' +pkg['name'] + '=' +pkg['version'] + '\' \\\n')
             else:
-                dockerfile.write('    \'' + pkg['name'] +'\' && conda clean -tipsy\n')
+                #latest or sepcific version
+                if(pkg['version'] == 'latest'):
+                    dockerfile.write('    \'' + pkg['name'] +'\' && conda clean -tipsy\n')
+                else:
+                    dockerfile.write('    \'' + pkg['name'] +'='+pkg['version']+'\' && conda clean -tipsy\n')
 
     dockerfile.write('USER $NB_USER\n')
 
 #execute the building process
-#str = subprocess.call('docker build -t ' + moduleName + ' .', shell=True)
+str = subprocess.call(['cd '+module_dir+ '; docker build -t ' + module_name + ' .'], shell=True)
