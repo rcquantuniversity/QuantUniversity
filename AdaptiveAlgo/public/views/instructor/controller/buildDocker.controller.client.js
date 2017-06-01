d94950c25b29cf4f1509112c24e24cee5643afd8
aranderia <ar.adaptivealgo@gmail.com>
2017-06-01 13:20:43 -0400

    build docker changes

--- AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
+++ AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
@@ -16,6 +16,17 @@
                 .then(
                     function (allPackages) {
                         vm.myData = allPackages.data;
+                        for(var i in vm.myData) {
+                            // console.log(vm.myData[i].base);
+                            if(vm.myData[i].base == "python2.7") {
+                                vm.py27 = vm.myData[i].packages;
+                            } else if (vm.myData[i].base == "python3.5") {
+                                vm.py35 = vm.myData[i].packages;
+                            } else if (vm.myData[i].base == "R") {
+                                vm.r = vm.myData[i].packages;
+                            }
+                        }                        
+                        // vm.py27 = vm.myData.packages;
                     },
                     function (err) {
                         vm.error = err;
@@ -26,8 +37,10 @@
 
         function createOutputJSON(packageList) {
             // packageList = {"name" : "python123456", "package" : "rohan"};
-            packageList = {"data":[{"_id":"592de4fc87aa4829f0fcca1b","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca1d","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1c","version":"latest"}],"base":"python2.7"},{"_id":"592de4fc87aa4829f0fcca1e","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca20","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1f","version":"latest"},{"_id":"592de54287aa4829f0fcca24","command":"pip install SciPy","name":"SciPy","version":"1.9"}],"base":"python3.5"},{"_id":"592de4fc87aa4829f0fcca21","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca23","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca22","version":"latest"}],"base":"R"}]};
+            // packageList = {"data":[{"_id":"592de4fc87aa4829f0fcca1b","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca1d","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1c","version":"latest"}],"base":"python2.7"},{"_id":"592de4fc87aa4829f0fcca1e","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca20","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1f","version":"latest"},{"_id":"592de54287aa4829f0fcca24","command":"pip install SciPy","name":"SciPy","version":"1.9"}],"base":"python3.5"},{"_id":"592de4fc87aa4829f0fcca21","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca23","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca22","version":"latest"}],"base":"R"}]};
 
+            console.log(packageList);
+            
             UserService
                 .createOutputJSON(packageList)
                 .then(
