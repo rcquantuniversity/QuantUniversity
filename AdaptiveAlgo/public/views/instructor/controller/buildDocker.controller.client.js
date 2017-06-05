df9aea645b7c12fd0d93f50b7feb6ad46785dce9
rcquantuniversity <rc.quantuniversity@gmail.com>
2017-06-05 11:26:31 -0400

    changed flow for creating docker image

--- AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
+++ AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
@@ -8,7 +8,7 @@
 
         var vm = this;
 
-        vm.createOutputJSON = createOutputJSON;
+        vm.createDockerImage = createDockerImage;
         vm.uploadToDockerHub = uploadToDockerHub;
         vm.showAllImages = showAllImages;
 
@@ -37,7 +37,7 @@
         }
         init();
 
-        function createOutputJSON(folder1, folder2, folder3, imageName) {
+        function createDockerImage(folder1, folder2, folder3, imageName) {
             // packageList = {"name" : "python123456", "package" : "rohan"};
             // packageList = {"data":[{"_id":"592de4fc87aa4829f0fcca1b","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca1d","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1c","version":"latest"}],"base":"python2.7"},{"_id":"592de4fc87aa4829f0fcca1e","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca20","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca1f","version":"latest"},{"_id":"592de54287aa4829f0fcca24","command":"pip install SciPy","name":"SciPy","version":"1.9"}],"base":"python3.5"},{"_id":"592de4fc87aa4829f0fcca21","__v":0,"packages":[{"name":"matplotlib","command":"pip install matplotlib","_id":"592de4fc87aa4829f0fcca23","version":"latest"},{"name":"numpy","command":"pip install numpy","_id":"592de4fc87aa4829f0fcca22","version":"latest"}],"base":"R"}]};
 
@@ -113,7 +113,8 @@
                             .createDockerImage()
                             .then(
                                 function (status) {
-                                    vm.message = "Docker Image created succesfully!";
+                                    vm.message = "Docker Image created successfully!";
+                                    $location.url("/instructor/listImages");
                                 },
                                 function (err) {
                                     vm.error = "Could not create Docker image. " + err;
