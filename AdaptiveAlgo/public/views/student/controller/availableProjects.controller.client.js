1480582d58a4f70edb405351fde92caff5bdacc4
aranderia <ar.adaptivealgo@gmail.com>
2017-06-09 15:13:39 -0400

    modal popup\

--- AdaptiveAlgo/public/views/student/controller/availableProjects.controller.client.js
+++ AdaptiveAlgo/public/views/student/controller/availableProjects.controller.client.js
@@ -3,14 +3,18 @@
         .module("AdaptiveAlgoApp")
         .controller("AvailableProjectsController", AvailableProjectsController);
 
-    function AvailableProjectsController($location, UserService, CourseService, $scope, $window) {
+    function AvailableProjectsController($location, UserService, CourseService, $scope, $window, ModalService) {
         var vm = this;
         vm.logout = logout;
         vm.runLab = runLab;
         vm.stopLab = stopLab;
 
+        vm.openModal = openModal;
+        vm.closeModal = closeModal;
+
         function init() {
             vm.isNotebookLoaded = false;
+            vm.bodyText = 'This text can be updated in modal 1';
             UserService
                 .listAllImagesForStudent()
                 .then(
@@ -24,6 +28,14 @@
                 );
         }
         init();
+
+        function openModal(id){
+            ModalService.Open(id);
+        }
+ 
+        function closeModal(id){
+            ModalService.Close(id);
+        }
 
         function stopLab(imageName) {
             CourseService
@@ -43,9 +55,10 @@
                 .startLab(imageName)
                 .then(
                     function (labURL) {
-                        vm.notebookUrl = 'https://'+labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",'')+'/user/a/notebooks/Untitled.ipynb';
-                        console.log(labURL);
-                        console.log(labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",''));
+                        // vm.notebookUrl = 'https://'+labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",'')+'/user/a/notebooks/Untitled.ipynb';
+                        vm.notebookUrl = 'https://www.google.com/';
+                        //console.log(labURL);
+                        //console.log(labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",''));
                         //vm.isNotebookLoaded = true;
                         console.log(vm.notebookUrl);
                         // $window.open(vm.notebookUrl, '_blank');
