458fb02d12446e5a3df3ff3d2df8c6a9c17644c8
rcquantuniversity <rc.quantuniversity@gmail.com>
2017-06-13 10:40:13 -0400

    changed notebook frame setting to hub

--- AdaptiveAlgo/public/views/student/controller/availableProjects.controller.client.js
+++ AdaptiveAlgo/public/views/student/controller/availableProjects.controller.client.js
@@ -55,7 +55,9 @@
                 .startLab(imageName)
                 .then(
                     function (labURL) {
-                        vm.notebookUrl = 'https://'+labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",'')+'/user/a/notebooks/Untitled.ipynb';
+                        // vm.notebookUrl = 'https://'+labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",'')+'/user/a/notebooks/Untitled.ipynb';
+                        vm.notebookUrl = 'https://'+labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",'');
+
                         // vm.notebookUrl = 'https://www.google.com/';
                         //console.log(labURL);
                         //console.log(labURL.data.replace('\\r\\n','').replace("\"",'').replace("\"",''));
