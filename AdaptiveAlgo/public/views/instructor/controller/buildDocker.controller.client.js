86fd6b6f59bf42c452bc81ec481b1db8efada748
aranderia <ar.adaptivealgo@gmail.com>
2017-06-13 16:31:13 -0400

    file upload

--- AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
+++ AdaptiveAlgo/public/views/instructor/controller/buildDocker.controller.client.js
@@ -1,7 +1,7 @@
 (function () {
     angular
     .module("AdaptiveAlgoApp")
-    .controller("BuildDockerController", function($scope, $http, UserService, $location, $window) {
+    .controller("BuildDockerController", function($scope, $http, UserService, $location, $window, Upload) {
         // $http.get("masterPackageJSON.json").then(function(response) {
         //     $scope.myData = response.data.data;
         // });
@@ -38,6 +38,33 @@
                 );
         }
         init();
+
+        vm.submit = function(){ //function to call on form submit
+            if (vm.upload_form.file.$valid && vm.file) { //check if from is valid
+                vm.upload(vm.file); //call upload function
+            }
+        }
+
+        vm.upload = function (file) {
+            Upload.upload({
+                url: 'http://localhost:3002/upload', //webAPI exposed to upload the file
+                data:{file:file} //pass file as data, should be user ng-model
+            }).then(function (resp) { //upload function returns a promise
+                if(resp.data.error_code === 0){ //validate success
+                    $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                } else {
+                    $window.alert('an error occured');
+                }
+            }, function (resp) { //catch error
+                console.log('Error status: ' + resp.status);
+                $window.alert('Error status: ' + resp.status);
+            }, function (evt) { 
+                console.log(evt);
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+                vm.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
+            });
+        };
 
         function addPackage(base, packageName, packageCommand, packageVersion) {
             var package = {
--- AdaptiveAlgo/server.js
+++ AdaptiveAlgo/server.js
@@ -19,6 +19,33 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var multer = require('multer');
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './private/services/uploads/')
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + file.originalname.replace(/\..+$/, '') + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+    }
+});
+
+var upload = multer({ //multer settings
+		        storage: storage
+		    }).single('file');
+
+/** API path that will upload the files */
+app.post('/upload', function(req, res) {
+    upload(req,res,function(err){
+        if(err){
+            res.json({error_code:1,err_desc:err});
+            return;
+        }
+        res.json({error_code:0,err_desc:null});
+    });
+});
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
