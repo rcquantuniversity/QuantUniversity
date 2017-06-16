639456cb2ddd1dfb45601f89914bda9118948321
adaptivejw <jw.adaptivealgo@gmail.com>
2017-06-16 16:21:50 -0400

    whitelist and load balancer

--- AdaptiveAlgo/private/services/aws_stop.py
+++ AdaptiveAlgo/private/services/aws_stop.py
@@ -4,6 +4,8 @@ import paramiko
 import time
 import json
 import os
+import sys
+import yaml
 from luigi.mock import MockFile
 from scp import SCPClient
 
@@ -26,12 +28,102 @@ class ParseParameters(luigi.Task):
         json.dump(params,_out)
         _out.close()
 
+class DummyLoadBalancer(luigi.Task):
+    task_namespace = 'aws'
+
+    def requires(self):
+        return ParseParameters()
+
+    def output(self):
+        return MockFile('LoadBalanceInfo')
+
+    def run(self):
+        #read from ParseParameters
+        with self.input().open('r') as infile:
+            print(infile)
+            params = json.load(infile)
+        
+        maxUsersForOneIns = 0
+        numOfCurrentUsers = 0
+        numOfExistingIns = 0
+        topIndex = 0
+        moduleName = params['Module']
+        userName = params['Username']
+        fileName = moduleName+'_balance_status.json'
+        whitelistName = 'whitelist.json'
+        service_dir = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+        jsonFilePath = service_dir+fileName
+        whitelistPath = service_dir+whitelistName
+
+        #if the JSON file doesnt exit
+        if not os.path.isfile(jsonFilePath):
+            print('corresponding JSON not here, exit')
+            sys.exit()
+
+        #load data from JSON file
+        with open(jsonFilePath, 'r') as f:
+            content = yaml.safe_load(f)
+
+        maxUsersForOneIns = int(content['maxUsers'])
+        topIndex = int(content['topIndex'])
+        vmdict = {}
+        for vm in content['vms']:
+            vmdict[str(vm)] = (content['vms'][vm])
+
+        #ckeck any unbalance in the cluster, if any conflict, exit the pipe line
+        for vm in vmdict:
+            #update the params
+            numOfExistingIns=numOfExistingIns+1
+            numOfCurrentUsers=numOfCurrentUsers+len(vmdict[vm])
+            if(len(vmdict[vm])>maxUsersForOneIns):
+                print('load balance in '+vm+' is broken')
+                sys.exit()
+
+        if(numOfExistingIns * maxUsersForOneIns < numOfCurrentUsers):
+            print('messed up with load balance, too many containers, too few instances.')
+            sys.exit()
+
+        #find del user
+        isFound = False
+        passInfo = {}
+        for vm in vmdict:
+            if(userName in vmdict[vm]):
+                isFound = True
+                print(userName+' is in '+vm+', find the coressponding IP for it')
+                vmdict[vm].remove(userName)
+                passInfo['Module'] = vm
+                with open(whitelistPath, 'w') as fp:
+                    whitelist={}
+                    whitelist['whitelist'] = vmdict[vm]
+                    json.dump(whitelist, fp)
+                if(len(vmdict[vm]) == 0):
+                    vmdict.pop(vm)
+                    print('the vm is empty now, ready to turn off')
+                break
+
+        if(isFound == False):
+            print('cannot find user: '+userName)
+            sys.exit()
+
+        #write to output for next 
+        _out = self.output().open('w')
+        json.dump(passInfo,_out)
+        _out.close()
+
+        #write data back to JSON file.
+        print(vmdict)
+        writeData = {}
+        writeData['maxUsers'] = maxUsersForOneIns
+        writeData['topIndex'] = topIndex
+        writeData['vms'] = vmdict
+        with open(jsonFilePath, 'w') as fp:
+            json.dump(writeData, fp)
 
 class GetInstanceIP(luigi.Task):
     task_namespace = 'aws'
 
     def requires(self):
-        return ParseParameters()
+        return DummyLoadBalancer()
 
     def output(self):
         return MockFile('InstanceInfo')
@@ -72,7 +164,7 @@ class StopTask(luigi.Task):
             print(infile)
             params = json.load(infile)
 
-        # read IP address from the out put
+        #read IP address from the out put
         with self.input()[1].open('r') as infile:
             ips = infile.read().splitlines()
         ip = ips[0]
@@ -85,12 +177,23 @@ class StopTask(luigi.Task):
         c.connect( hostname = ip, username = 'ec2-user', pkey = k)
         print ('connected')
         
-        
-        #stop Jupyterhub service and all containers
+        #stop a single container
         if (params['Approach']) == 'Exit':
             #get container id
             USERNAME = params['Username']
             USER_DIR_NAME = 'jhub-'+USERNAME
+            #1. update white list and backup directory
+            with SCPClient(c.get_transport()) as scp:
+                scp.put('C:\\Users\\QuantUniversity-6\\Rohan\\QuantUniversity\\AdaptiveAlgo\\private\\services\\whitelist.json', '/home/ec2-user/jupyterhub-deploy-docker')
+            commands = ['sudo mv /home/ec2-user/nbdata/'+USER_DIR_NAME+' /home/ec2-user/nbdata/'+USER_DIR_NAME+'bak',
+                        'sudo cp /home/ec2-user/jupyterhub-deploy-docker/whitelist.json /var/lib/docker/volumes/jupyterhub-data/_data']
+            for command in commands:
+                print ('Executing {}'.format( command ))
+                stdin , stdout, stderr = c.exec_command(command)
+                print (stdout.read())
+                print ('Errors')
+                print (stderr.read())
+            #2. check the container status
             commands = ['docker ps -aqf \"name=' + 'jupyter-' + USERNAME +'\"']
             for command in commands:
                 print ('Executing {}'.format( command ))
@@ -105,14 +208,17 @@ class StopTask(luigi.Task):
                 CONTAINER_ID = returnVal.decode("utf-8")
                 print ('Errors')
                 print (stderr.read())
+            #3. if the container is there, rm the container
             commands = ['docker rm -f ' + CONTAINER_ID, 
-                        'sudo mv /home/ec2-user/nbdata/'+USER_DIR_NAME+' /home/ec2-user/nbdata/'+USER_DIR_NAME+'bak']
+                        'sudo mv /home/ec2-user/nbdata/'+USER_DIR_NAME+' /home/ec2-user/nbdata/'+USER_DIR_NAME+'bak',
+                        'sudo cp /home/ec2-user/jupyterhub-deploy-docker/whitelist.json /var/lib/docker/volumes/jupyterhub-data/_data']
             for command in commands:
                 print ('Executing {}'.format( command ))
                 stdin , stdout, stderr = c.exec_command(command)
                 print (stdout.read())
                 print ('Errors')
                 print (stderr.read())
+        
         #terminate the AWS instance
         if (params['Approach']) == 'Terminate':
             ec2 = boto3.resource('ec2')
@@ -121,6 +227,7 @@ class StopTask(luigi.Task):
             for instance in instances:
                 target_ins=instance
             target_ins.terminate()
+        
         #shutdown the service
         if (params['Approach']) == 'Down':
             commands = [ 'ls', 'cd jupyterhub-deploy-docker; docker-compose down']
@@ -130,6 +237,8 @@ class StopTask(luigi.Task):
                 print (stdout.read())
                 print ('Errors')
                 print (stderr.read())
+
         c.close()
+
 if __name__ == '__main__':
     luigi.run(['aws.StopTask', '--local-scheduler'])
