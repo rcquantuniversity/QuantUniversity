c4c2387996cd10b7fe0c4abd648de090f4896a19
adaptivejw <jw.adaptivealgo@gmail.com>
2017-06-22 13:08:20 -0400

    hardcoded variables

---
+++ AdaptiveAlgo/private/services/rstudio_stop.py
@@ -0,0 +1,159 @@
+import boto3
+import luigi
+import paramiko
+import time
+import json
+import os
+import sys
+import yaml
+from luigi.mock import MockFile
+from scp import SCPClient
+
+class ParseParameters(luigi.Task):
+    task_namespace = 'aws'
+
+    def output(self):
+        return MockFile('InstanceParams')
+
+    def run(self):
+        service_dir = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+        with open(service_dir+'/stop_params.json') as data_file:    
+            data = json.load(data_file)
+        params = dict()
+        params['Approach'] = 'Terminate'
+        params['Module'] = 'rstudio-rohan'
+        params['Username'] = 'rohan'
+        print(params)
+        _out = self.output().open('w')
+        json.dump(params,_out)
+        _out.close()
+
+
+class GetInstanceIP(luigi.Task):
+    task_namespace = 'aws'
+
+    def requires(self):
+        return ParseParameters()
+
+    def output(self):
+        return MockFile('InstanceInfo')
+
+    def run(self):
+        
+        task_namespace = 'aws'
+
+        with self.input().open('r') as infile:
+            print(infile)
+            params = json.load(infile)
+        
+        ec2 = boto3.resource('ec2')
+        filters = [{'Name':'tag:Name', 'Values':[params['Module']]}]
+        instances = ec2.instances.filter(Filters=filters)
+        counter = 0
+        for instance in instances:
+            counter = counter + 1
+            target_ins=instance
+        if(counter==0):
+            print('No module named '+params['Module'] + ' exit.')
+            return -1
+        print(target_ins.id, target_ins.instance_type, target_ins.public_ip_address)
+        
+        _out = self.output().open('w')
+        _out.write(target_ins.public_ip_address)
+        _out.close()
+
+class StopTask(luigi.Task):
+    task_namespace = 'aws'
+
+    def requires(self):
+        return [ParseParameters(), GetInstanceIP()]
+
+    def run(self):
+        #read stopping approach
+        with self.input()[0].open('r') as infile:
+            print(infile)
+            params = json.load(infile)
+
+        #read IP address from the out put
+        with self.input()[1].open('r') as infile:
+            ips = infile.read().splitlines()
+        ip = ips[0]
+        print(ip)
+        #make ssh connection
+        k = paramiko.RSAKey.from_private_key_file('/home/parallels/.ssh/adaptivealgo.pem')
+        c = paramiko.SSHClient()
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print ('connecting')
+        c.connect( hostname = ip, username = 'ec2-user', pkey = k)
+        print ('connected')
+        
+        #stop a single container
+        if (params['Approach']) == 'Exit':
+            #get container id
+            USERNAME = params['Username']
+            USER_DIR_NAME = 'jhub-'+USERNAME
+            
+            #2. check the container status
+            commands = ['docker ps -aqf \"name=' + 'jupyter-' + USERNAME +'\"']
+            for command in commands:
+                print ('Executing {}'.format( command ))
+                stdin , stdout, stderr = c.exec_command(command)
+                returnVal = stdout.read()
+                print (returnVal)
+                if(returnVal.decode("utf-8") == ''):
+                    print('no such container named ' + USERNAME)
+                    c.close()
+                    return
+                print('container found')
+                CONTAINER_ID = returnVal.decode("utf-8")
+                print ('Errors')
+                print (stderr.read())
+            #3. if the container is there, rm the container
+            commands = ['docker rm -f ' + CONTAINER_ID, 
+                        'sudo mv /home/ec2-user/nbdata/'+USER_DIR_NAME+' /home/ec2-user/nbdata/'+USER_DIR_NAME+'bak',
+                        'sudo cp /home/ec2-user/jupyterhub-deploy-docker/whitelist.json /var/lib/docker/volumes/jupyterhub-data/_data']
+            for command in commands:
+                print ('Executing {}'.format( command ))
+                stdin , stdout, stderr = c.exec_command(command)
+                print (stdout.read())
+                print ('Errors')
+                print (stderr.read())
+        
+        #terminate the AWS instance
+        if (params['Approach']) == 'Terminate':
+            #1. check the container status
+            commands = ['docker ps -aqf \"name=rocker_rstudio' + '\"']
+            for command in commands:
+                print ('Executing {}'.format( command ))
+                stdin , stdout, stderr = c.exec_command(command)
+                returnVal = stdout.read()
+                print (returnVal)
+                if(returnVal.decode("utf-8") == ''):
+                    print('no such container named ' + USERNAME)
+                    c.close()
+                    return
+                print('container found')
+                CONTAINER_ID = returnVal.decode("utf-8")
+                print ('Errors')
+                print (stderr.read())
+            #2. if the container is there, rm the container
+            commands = ['docker rm -f ' + CONTAINER_ID]
+            for command in commands:
+                print ('Executing {}'.format( command ))
+                stdin , stdout, stderr = c.exec_command(command)
+                print (stdout.read())
+                print ('Errors')
+                print (stderr.read())
+            #3. terminate instance
+            ec2 = boto3.resource('ec2')
+            filters = [{'Name':'tag:Name', 'Values':[params['Module']]}]
+            instances = ec2.instances.filter(Filters=filters)
+            for instance in instances:
+                target_ins=instance
+            target_ins.terminate()
+        
+
+        c.close()
+
+if __name__ == '__main__':
+    luigi.run(['aws.StopTask', '--local-scheduler'])
